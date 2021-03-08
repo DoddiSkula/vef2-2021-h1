5b5cfa42222233044cbcfa154b11fd7bda04165e
Doddi <doddiskula@gmail.com>
2021-03-08 16:17:06 +0000

    db.js grunnur

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import express from 'express';
+// import session from 'express-session';
+// import { router as tvRouter } from './tv.js';
+// import { router as userRouter } from './users.js';
+
+const app = express();

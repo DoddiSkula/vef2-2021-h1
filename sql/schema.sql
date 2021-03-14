9a13cfcf0ccb528cf5802753ce3ec1c72c7fdb2f
oscar6662 <oscar.berenguer6@gmail.com>
2021-03-14 23:29:16 +0000

    setup

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS shows (
   show_aired date,
   inproduction boolean,
   tagline varchar(128),
-  show_description varchar(256),
+  show_description varchar(65536),
   show_language varchar(128),
   network varchar(128),
   webpage varchar(128)
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS show_genre;
 CREATE TABLE IF NOT EXISTS show_genre (
   id serial primary key,
   show_id bigint,
-  genre varchar(64),
+  genre integer,
   constraint show_id foreign key (show_id) REFERENCES shows(id),
   constraint genre foreign key (genre) REFERENCES genre(id)
 );
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS season (
   season_aired date,
   season_description varchar(256),
   poster text not null,
-  show varchar(128) not null,
+  show integer not null,
   constraint show foreign key (show) REFERENCES shows(id),
   constraint nr_biggerthanzero check (nr > 0)
 );
@@ -75,10 +75,9 @@ DROP TABLE IF EXISTS info;
 CREATE TABLE IF NOT EXISTS info  (
   id serial primary key,
   show integer not null,
-  user integer not null,//Hérna er villa :(
+  usr integer not null,
   watch_state varchar(48),
   rating integer,
   constraint show foreign key (show) REFERENCES shows(id),
-  constraint user foreign key (user) REFERENCES users(id)
+  constraint usr foreign key (usr) REFERENCES users(id)
 );
-

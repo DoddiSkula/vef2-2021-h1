f77ffe825902cc5dd96f46db8f091e9858be00ba
kak25 <kak25@hi.is>
2021-03-17 12:56:53 +0000

    lítill breyting á get /tv nær í allar upplysingar

--- src/tv.js
+++ src/tv.js
@@ -1,43 +1,45 @@
-import express from 'express';
-import { query } from './db.js';
+import express from "express";
+import { query } from "./db.js";
 export const router = express.Router();
 
-router.get('/tv', async (req, res) => {
-  const q = 'SELECT show_name, show_description FROM shows';
+router.get("/tv", async (req, res) => {
+  const q = "SELECT* FROM shows";
   const results = await query(q);
   return res.json(results.rows);
 });
 
-router.get('/tv/:id', async (req, res) => {
-  const show = 'SELECT show_name, show_description FROM shows WHERE id = $1';
+router.get("/tv/:id", async (req, res) => {
+  const show = "SELECT show_name, show_description FROM shows WHERE id = $1";
   const showq = await query(show, [req.params.id]);
-  const seasons = 'SELECT season_name, season_description FROM season WHERE show_id = $1';
+  const seasons =
+    "SELECT season_name, season_description FROM season WHERE show_id = $1";
   const seasonsq = await query(seasons, [req.params.id]);
 
   return res.json([showq.rows, seasonsq.rows]);
 });
 
-router.get('/tv/:id/season', async (req, res) => {
-  const seasons = 'SELECT season_name, season_description FROM season WHERE show_id = $1';
+router.get("/tv/:id/season", async (req, res) => {
+  const seasons =
+    "SELECT season_name, season_description FROM season WHERE show_id = $1";
   const seasonsq = await query(seasons, [req.params.id]);
 
   return res.json(seasonsq.rows);
 });
 
-router.get('/tv/:id/season/:id', async (req, res) => {
+router.get("/tv/:id/season/:id", async (req, res) => {
   //TODO
 });
 
-router.get('/tv/:id/season/:id/episode', async (req, res) => {
+router.get("/tv/:id/season/:id/episode", async (req, res) => {
   //TODO
 });
 
-router.get('/tv/:id/season/:id/episode/:id', async (req, res) => {
+router.get("/tv/:id/season/:id/episode/:id", async (req, res) => {
   //TODO
 });
 
-router.get('/genres', async (req, res) => {
-  const q = 'SELECT genre_name FROM genre';
+router.get("/genres", async (req, res) => {
+  const q = "SELECT genre_name FROM genre";
   const results = await query(q);
   return res.json(results.rows);
 });

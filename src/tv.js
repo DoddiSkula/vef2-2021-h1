08e8e88d4f650fb7a2f078e9342d1a1be4379dd8
oscar6662 <oscar.berenguer6@gmail.com>
2021-03-17 02:34:26 +0000

    a bit more tv

--- src/tv.js
+++ src/tv.js
@@ -9,9 +9,31 @@ router.get('/tv', async (req, res) => {
 });
 
 router.get('/tv/:id', async (req, res) => {
-  const q = 'SELECT show_name, show_description FROM shows WHERE id = $1';
-  const results = await query(q, [req.params.id]);
-  return res.json(results.rows);
+  const show = 'SELECT show_name, show_description FROM shows WHERE id = $1';
+  const showq = await query(show, [req.params.id]);
+  const seasons = 'SELECT season_name, season_description FROM season WHERE show_id = $1';
+  const seasonsq = await query(seasons, [req.params.id]);
+
+  return res.json([showq.rows, seasonsq.rows]);
+});
+
+router.get('/tv/:id/season', async (req, res) => {
+  const seasons = 'SELECT season_name, season_description FROM season WHERE show_id = $1';
+  const seasonsq = await query(seasons, [req.params.id]);
+
+  return res.json(seasonsq.rows);
+});
+
+router.get('/tv/:id/season/:id', async (req, res) => {
+  //TODO
+});
+
+router.get('/tv/:id/season/:id/episode', async (req, res) => {
+  //TODO
+});
+
+router.get('/tv/:id/season/:id/episode/:id', async (req, res) => {
+  //TODO
 });
 
 router.get('/genres', async (req, res) => {
